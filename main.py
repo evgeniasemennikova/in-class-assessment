fc1ede099f726553506c6166d59c7929dc5711ca
evgeniasemennikova <evgeniasemennikova1@gmail.com>
2024-04-08 11:31:06 +0200

    semennikova_submission

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 """
 PowerOfTen
 """
-# Provide your solution here
+number = int(input("Enter a max three-digit number:"))
+if number < 0:
+    print("The number cannot be negativ.")
+elif number > 999:
+    print("The number cannot have more than three digits.")
+else:
+    decomposed = number % 10
+    print(f"{number} = {decomposed}")
+
+
+
+
 
 
 """
 Cash Box
 """
-# Provide your solution here
+to_pay = int(input("To pay:"))
+if to_pay < 0:
+    print("Negativ payment is invalid.")
+while to_pay < 0:
+    int(input("To pay:"))
+    break
+recieved = int(input("Recieved:"))
+if recieved > to_pay:
+    change = recieved - to_pay
+    print(f"Your change is: {change}.")
+else:
+    print("You did not pay enough.")
+
